9875bf35507915b4ec89119648d639526b893548
yeexuen <56750868+yeexuen@users.noreply.github.com>
2022-06-07 11:27:20 +0800

    update

--- src/app/task-detail/task-detail.page.ts
+++ src/app/task-detail/task-detail.page.ts
@@ -31,6 +31,122 @@ export class TaskDetailPage implements OnInit {
     console.log(this.info, this.img);
 
   }
+  Header = []
+  HeaderChoice = ''
+  HeaderSingle = false
+  HeaderRipple = false
+  HeaderDouble = false
+  HeaderFrench = false
+  HeaderEyelet = false
+
+  RomanBlinds = false
+  RollerBlinds = false
+  ZebraBlinds = false
+  WoodenBlinds = false
+  blindsChoice = ''
+
+  PlainWall = false
+  FabricWall = false
+  NonWovenWall = false
+  PatternWall = false
+  VinylWall = false
+  WallpaperChoice = ''
+
+  typeChanged() {
+    this.HeaderSingle = false
+    this.HeaderRipple = false
+    this.HeaderDouble = false
+    this.HeaderFrench = false
+    this.HeaderEyelet = false
+    this.HeaderChoice = ''
+
+    this.RomanBlinds = false
+    this.RollerBlinds = false
+    this.ZebraBlinds = false
+    this.WoodenBlinds = false
+    this.blindsChoice = ''
+
+    this.PlainWall = false
+    this.FabricWall = false
+    this.NonWovenWall = false
+    this.PatternWall = false
+    this.VinylWall = false
+    this.WallpaperChoice = ''
+  }
+
+  HeaderSelection(x) {
+    this.HeaderSingle = false
+    this.HeaderRipple = false
+    this.HeaderDouble = false
+    this.HeaderFrench = false
+    this.HeaderEyelet = false
+
+    if (x == 'Single Pleat') {
+      this.HeaderSingle = true
+      this.HeaderChoice = 'Single Pleat'
+    } else if (x == 'Ripple Fold') {
+      this.HeaderRipple = true
+      this.HeaderChoice = 'Ripple Fold'
+    } else if (x == 'Double Pleat') {
+      this.HeaderDouble = true
+      this.HeaderChoice = 'Double Pleat'
+    } else if (x == 'French Pleat') {
+      this.HeaderFrench = true
+      this.HeaderChoice = 'French Pleat'
+    } else if (x == 'Eyelet') {
+      this.HeaderEyelet = true
+      this.HeaderChoice = 'Eyelet'
+    }
+
+  }
+
+  blindsSelection(x) {
+    this.RomanBlinds = false
+    this.RollerBlinds = false
+    this.ZebraBlinds = false
+    this.WoodenBlinds = false
+
+    if (x == 'Roman Blinds') {
+      this.RomanBlinds = true
+      this.blindsChoice = 'Roman Blinds'
+    } else if (x == 'Roller Blinds') {
+      this.RollerBlinds = true
+      this.blindsChoice = 'Roller Blinds'
+    } else if (x == 'Zebra Blinds') {
+      this.ZebraBlinds = true
+      this.blindsChoice = 'Zebra Blinds'
+    } else if (x == 'Wooden Blinds') {
+      this.WoodenBlinds = true
+      this.blindsChoice = 'Wooden Blinds'
+    }
+
+  }
+
+  wallpaperSelection(x) {
+    this.PlainWall = false
+    this.FabricWall = false
+    this.NonWovenWall = false
+    this.PatternWall = false
+    this.VinylWall = false
+
+    if (x == 'Plain') {
+      this.PlainWall = true
+      this.WallpaperChoice = 'Plain'
+    } else if (x == 'Fabric') {
+      this.FabricWall = true
+      this.WallpaperChoice = 'Fabric'
+    } else if (x == 'Non-Woven') {
+      this.NonWovenWall = true
+      this.WallpaperChoice = 'Non-Woven'
+    } else if (x == 'Patterned') {
+      this.PatternWall = true
+      this.WallpaperChoice = 'Patterned'
+    } else if (x == 'Vinyl') {
+      this.VinylWall = true
+      this.WallpaperChoice = 'Vinyl'
+    }
+
+  }
 
   async addTask() {
     const modal = await this.modal.create({
