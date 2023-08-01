fd44b6bfc50aa90515f3e9e0fc3c66ed08f00dd1
hawquan <hawquan@gmail.com>
2023-08-01 19:35:40 +0800

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Market } from '@ionic-native/market/ngx';
 import { Platform } from '@ionic/angular';
 import { FCM } from 'cordova-plugin-fcm-with-dependecy-updated/ionic/ngx';
 import Swal from 'sweetalert2';
+import { SafariViewController } from '@awesome-cordova-plugins/safari-view-controller/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-root',
@@ -23,9 +25,11 @@ export class AppComponent {
     private market: Market,
     private platform: Platform,
     private fcm: FCM,
+    private safariViewController: SafariViewController,
+    private inAppBrowser: InAppBrowser
   ) {
 
-    let version = '000035' 
+    let version = '000035'
     // ionic cordova build android --release -- -- --packageType=bundle
     // jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore curtain.jks app-release.aab curtain
     // curtain12345
@@ -92,9 +96,33 @@ export class AppComponent {
                 allowOutsideClick: false,
                 allowEscapeKey: false,
               }).then((a) => {
-                if (a.isConfirmed) {
-                  window.open("https://apps.apple.com/us/app/curtain-library/id1645866855", "_system");
-                }
+                // if (a.isConfirmed) {
+                //   window.open("https://apps.apple.com/us/app/curtain-library/id1645866855", "_system");
+                // }
+
+                // this.safariViewController.isAvailable()
+                //   .then(async (available: boolean) => {
+                //     if (available) {
+
+                //       this.safariViewController.show({
+                //         url: "https://apps.apple.com/us/app/curtain-library/id1645866855",
+                //       })
+                //         .subscribe((result: any) => {
+                //           if (result.event === 'opened') console.log('Opened');
+                //           else if (result.event === 'loaded') console.log('Loaded');
+                //           else if (result.event === 'closed') console.log('Closed');
+                //         },
+                //           (error: any) => console.error(error)
+                //         );
+
+                //     } else {
+                //       window.open("https://apps.apple.com/us/app/curtain-library/id1645866855", '_system');
+                //       // use fallback browser, example InAppBrowser
+                //     }
+                //   }).catch(async (error) => {
+                //     window.open("https://apps.apple.com/us/app/curtain-library/id1645866855", '_system');
+                //   })
+                this.openURL()
               })
             }
 
@@ -113,7 +141,36 @@ export class AppComponent {
       })
     })
 
+  }
 
+  async openURL() {
+    try {
+      const url = 'https://apps.apple.com/us/app/curtain-library/id1645866855';
+      const target = '_system';
+      const options: string = 'location=yes,toolbar=yes,closebuttoncaption=Close';
+
+      const browser = this.inAppBrowser.create(url, target, options);
+
+      browser.on('loadstart').subscribe((event) => {
+        // Do something when the URL starts loading.
+      });
+
+      browser.on('loadstop').subscribe((event) => {
+        // Do something when the URL has finished loading.
+      });
+
+      browser.on('loaderror').subscribe((event) => {
+        // Handle any error that occurs while loading the URL.
+        console.error(event);
+      });
+
+      browser.on('exit').subscribe(() => {
+        // Handle when the InAppBrowser is closed.
+      });
+    } catch (error) {
+      // Handle any other errors that may occur.
+      console.error(error);
+    }
   }
 
   goMarket() {
